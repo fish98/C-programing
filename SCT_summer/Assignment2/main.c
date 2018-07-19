13ec5d0fc264db9d94c5086d6f502e569ff4ce35
fish98 <jiongchiyu@gmail.com>
2018-07-19 13:34:02 +0800

    add assignment2

--- SCT_summer/Assignment2/main.c
+++ SCT_summer/Assignment2/main.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <mpi.h>
 
 #include "matrix.h"
 
-int main(){
+int main(int argc, char** argv){
 
     char operator;
     clock_t start, finish;
@@ -18,6 +19,12 @@ int main(){
     ReadMatrix("./matrix1", M1);
     ReadMatrix("./matrix2", M2);
 
+    MPI_Init(NULL, NULL);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+
     printf("Load Matrix Done\n");
 
     printf("Choose Operator : '+ or * or f'(enter q for quit)\n");
@@ -26,7 +33,8 @@ int main(){
     while(operator != 'q'){
         switch(operator){
 
-        case '+': start = clock(); 
+        case '+': 
+            start = clock(); 
             AddMatrix(M1, M2, M3);
             finish  = clock();
             printf("Use %f Seconds To Finish Add Operation\n", (double)(finish-start)/1000);
@@ -48,4 +56,6 @@ int main(){
     }
         scanf("%c", &operator);
     }
+
+    MPI_Finalize();
 }
--- SCT_summer/Assignment2/matrix.c
+++ SCT_summer/Assignment2/matrix.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <mpi.h>
 
 #include "matrix.h"
 
@@ -55,6 +56,14 @@ void ReadMatrix(char * Filename, Matrix * M){
     while(fscanf(fp, "%lf", M->data+prefix++)!= EOF);
 }
 
+void MPI_AddMatrix(Matrix * M1, Matrix * M2){
+    
+
+
+
+}
+
+
 void AddMatrix(Matrix * M1, Matrix * M2, Matrix * M3){
     int i;
 
@@ -74,74 +83,74 @@ void AddMatrix(Matrix * M1, Matrix * M2, Matrix * M3){
     for(i = 0; i<M3->col * M3->row; i++){
         *(M3->data+i) = *(M1->data+i)+*(M2->data+i);
         }
-    printf("Output for Add Operation\n");
-    PrintMatrix(M3);
-    printf("Choose Operator '+ or * or f' (enter q for quit)\n");
+    // printf("Outn put for Add Operation\n");
+    // PrintMatrix(M3);
+    // printf("Choose Operator '+ or * or f' (enter q for quit)\n");
     }
 }
 
-void MiltiplyMatrix(Matrix * M1, Matrix * M2, Matrix * M3){
-    int i, j, k;
+// void MPI_MiltiplyMatrix(Matrix * M1, Matrix * M2, Matrix * M3){
+//     int i, j, k;
 
-    // Judge First
+//     // Judge First
 
-    if (M1->row != M2->col){
-        printf("Invalid Input of Matrix\n");
-        printf("Choose Operator (enter q for quit)\n");
-    } else
-    {
-    // Init M3
+//     if (M1->row != M2->col){
+//         printf("Invalid Input of Matrix\n");
+//         printf("Choose Operator (enter q for quit)\n");
+//     } else
+//     {
+//     // Init M3
 
-    M3->col = M1->col;
-    M3->row = M2->row;
-    M3->data = malloc(sizeof(double) * M3->col * M3->row);
-    }
-    for(i = 0; i< M3->col; i++){
-        for(j = 0; j< M3-> row;j++){
-            int result = 0;
-            for(k = 0; k < M1->row; k++){
-               result += *(M1->data+i*M1->row+k) * *(M2->data+k*M2->row+j);
-            }
-            *(M3->data+i*M3->row+j) = result;
-        }
-    }
-    printf("Output for Miltiply Operation\n");
-    PrintMatrix(M3);
-    printf("Choose Operator '+ or * or f' (enter q for quit)\n"); 
-}
+//     M3->col = M1->col;
+//     M3->row = M2->row;
+//     M3->data = malloc(sizeof(double) * M3->col * M3->row);
+//     }
+//     for(i = 0; i< M3->col; i++){
+//         for(j = 0; j< M3-> row;j++){
+//             int result = 0;
+//             for(k = 0; k < M1->row; k++){
+//                result += *(M1->data+i*M1->row+k) * *(M2->data+k*M2->row+j);
+//             }
+//             *(M3->data+i*M3->row+j) = result;
+//         }
+//     }
+//     printf("Output for Miltiply Operation\n");
+//     PrintMatrix(M3);
+//     printf("Choose Operator '+ or * or f' (enter q for quit)\n"); 
+// }
 
-void FunctionMatrix(Matrix * M1, Matrix * M3){
+// void MPI_FunctionMatrix(Matrix * M1, Matrix * M3){
 
-    // Init M3
+//     // Init M3
 
-    int i, power;
-    M3->col = M1->col;
-    M3->row = M1->row;
-    M3->data = malloc(sizeof(double) * M3->col * M3->row);
-    for(i = 0; i < M3->col * M3->row; i++){
-            *(M3->data+i) = *(M1->data+i);
-        }
+//     int i, power;
+//     M3->col = M1->col;
+//     M3->row = M1->row;
+//     M3->data = malloc(sizeof(double) * M3->col * M3->row);
+//     for(i = 0; i < M3->col * M3->row; i++){
+//             *(M3->data+i) = *(M1->data+i);
+//         }
 
-    printf("enter the power of the operation element\n");
+//     printf("enter the power of the operation element\n");
 
-    scanf("%d", &power);
-    if(power > 0){
-        for(i = 0; i < M3->col * M3->row; i++){
-            *(M3->data+i) = pow(*(M3->data+i), power);
-        }
-    }
-    else if(power == 0){
-         for(i = 0; i < M3->col * M3->row; i++){
-            *(M3->data+i) = 1;
-        }
-    }
-    else {
-        power = -power;
-        for(i = 0; i < M3->col * M3->row; i++){
-            *(M3->data+i) = 1 / pow(*(M3->data+i), power);
-        }
-    }
-    printf("Output for Miltiply Operation\n");
-    PrintMatrix(M3);
-    printf("Choose Operator '+ or * or f' (enter q for quit)\n"); 
-}
+//     scanf("%d", &power);
+//     if(power > 0){
+//         for(i = 0; i < M3->col * M3->row; i++){
+//             *(M3->data+i) = pow(*(M3->data+i), power);
+//         }
+//     }
+//     else if(power == 0){
+//          for(i = 0; i < M3->col * M3->row; i++){
+//             *(M3->data+i) = 1;
+//         }
+//     }
+//     else {
+//         power = -power;
+//         for(i = 0; i < M3->col * M3->row; i++){
+//             *(M3->data+i) = 1 / pow(*(M3->data+i), power);
+//         }
+//     }
+//     printf("Output for Miltiply Operation\n");
+//     PrintMatrix(M3);
+//     printf("Choose Operator '+ or * or f' (enter q for quit)\n"); 
+// }
